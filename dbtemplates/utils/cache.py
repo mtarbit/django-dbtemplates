7424ef372b3e93eccdbfec5aa8c2b3d3c1cb1aee
Matt Tarbit <mtarbit@gmail.com>
2021-04-14 15:18:59 +0100

    Add support for django 3.2.

--- dbtemplates/utils/cache.py
+++ dbtemplates/utils/cache.py
@@ -7,10 +7,14 @@ from dbtemplates.conf import settings
 
 def get_cache_backend():
     """
-    Compatibilty wrapper for getting Django's cache backend instance
+    Compatibility wrapper for getting Django's cache backend instance
     """
-    from django.core.cache import _create_cache
-    cache = _create_cache(settings.DBTEMPLATES_CACHE_BACKEND)
+    try:  # django 3.1 and below
+        from django.core.cache import _create_cache
+        cache = _create_cache(settings.DBTEMPLATES_CACHE_BACKEND)
+    except ImportError:  # django 3.2 and above
+        from django.core.cache import caches
+        cache = caches[settings.DBTEMPLATES_CACHE_BACKEND]
     # Some caches -- python-memcached in particular -- need to do a cleanup at
     # the end of a request cycle. If not implemented in a particular backend
     # cache.close is a no-op
